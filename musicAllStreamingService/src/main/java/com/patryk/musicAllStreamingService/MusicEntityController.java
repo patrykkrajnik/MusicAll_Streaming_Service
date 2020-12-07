b1f5fcfdbccdf63a28fdeb8e805a41c0df8e2e61
patrykkrajnik <patrykkrajnik@icloud.com>
2020-12-07 20:58:43 +0100

    builidng RestController

--- musicAllStreamingService/src/main/java/com/patryk/musicAllStreamingService/MusicEntityController.java
+++ musicAllStreamingService/src/main/java/com/patryk/musicAllStreamingService/MusicEntityController.java
@@ -1,4 +1,56 @@
 package com.patryk.musicAllStreamingService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class MusicEntityController {
+
+    private static final String FILES_PATH = "your local path to directory with music files";
+
+    @Autowired
+    MusicRepository musicRepository;
+
+    @RequestMapping(value = "/music", method = RequestMethod.GET)
+    public ResponseEntity<List<MusicEntity>> getAllSongs(@RequestParam(required = false) String songName) {
+        try {
+            List<MusicEntity> musicEntities = new ArrayList<MusicEntity>();
+
+            if (songName == null) {
+                musicRepository.findAll().forEach(musicEntities::add);
+            } else {
+                musicRepository.findBySongName(songName).forEach(musicEntities::add);
+            }
+
+            if (musicEntities.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(musicEntities, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(value = "/music/{title}", method = RequestMethod.GET)
+    public void getSongAsByteArray(@PathVariable("title") final String title, HttpServletResponse response) throws IOException {
+        File file = new File(FILES_PATH + title);
+        InputStream in = new FileInputStream(file);
+        response.setContentType("audio/mp3");
+        response.setContentLength((int) file.length());
+
+        FileCopyUtils.copy(in, response.getOutputStream());
+    }
 }
--- musicAllStreamingService/src/main/java/com/patryk/musicAllStreamingService/MusicRepository.java
+++ musicAllStreamingService/src/main/java/com/patryk/musicAllStreamingService/MusicRepository.java
@@ -1,4 +1,11 @@
 package com.patryk.musicAllStreamingService;
 
-public interface MusicRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MusicRepository extends CrudRepository<MusicEntity, Integer> {
+    List<MusicEntity> findBySongName(String songName);
 }
