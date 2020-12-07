e0b19de98fa7c51694dcbf9eaf48ab18c4b460ce
patrykkrajnik <patrykkrajnik@icloud.com>
2020-12-07 20:39:06 +0100

    initializing a project

---
+++ musicAllStreamingService/src/main/java/com/patryk/musicAllStreamingService/MusicAllStreamingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.patryk.musicAllStreamingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicAllStreamingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicAllStreamingServiceApplication.class, args);
+	}
+
+}
---
+++ musicAllStreamingService/src/test/java/com/patryk/musicAllStreamingService/MusicAllStreamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patryk.musicAllStreamingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAllStreamingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
