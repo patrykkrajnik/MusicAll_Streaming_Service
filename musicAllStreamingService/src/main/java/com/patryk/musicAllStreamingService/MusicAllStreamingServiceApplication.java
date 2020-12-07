d9d2f4f2c02a85aa990d0db28c3f2f67ab65e538
patrykkrajnik <patrykkrajnik@icloud.com>
2020-12-07 21:03:59 +0100

    builidng main Bean

--- musicAllStreamingService/src/main/java/com/patryk/musicAllStreamingService/MusicAllStreamingServiceApplication.java
+++ musicAllStreamingService/src/main/java/com/patryk/musicAllStreamingService/MusicAllStreamingServiceApplication.java
@@ -1,7 +1,11 @@
 package com.patryk.musicAllStreamingService;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.File;
 
 @SpringBootApplication
 public class MusicAllStreamingServiceApplication {
@@ -10,4 +14,18 @@ public class MusicAllStreamingServiceApplication {
 		SpringApplication.run(MusicAllStreamingServiceApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner saveData(MusicRepository musicRepository) {
+		String DIRECTORY_PATH = "your local path to directory with music files";
+		File files = new File(DIRECTORY_PATH);
+		return (args -> {
+			for (File file : files.listFiles()) {
+				if (file.isFile() & !file.getName().equals(".DS_Store")) {
+					String baseName = file.getName().replaceAll("\\.[^.]*$", "");
+					musicRepository.save(new MusicEntity(baseName, "http://localhost:8080/api/music/" + baseName + ".mp3"));
+					System.out.println(baseName);
+				}
+			}
+		});
+	}
 }
