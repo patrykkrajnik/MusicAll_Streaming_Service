92c8bc5cd46dff8fbd0b9d1cbadf9d699b27a462
patrykkrajnik <patrykkrajnik@icloud.com>
2020-12-07 20:51:16 +0100

    building MusicEntity class

--- musicAllStreamingService/src/main/java/com/patryk/musicAllStreamingService/MusicEntity.java
+++ musicAllStreamingService/src/main/java/com/patryk/musicAllStreamingService/MusicEntity.java
@@ -1,4 +1,34 @@
 package com.patryk.musicAllStreamingService;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class MusicEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idSong;
+
+    private String songName;
+    private String songURL;
+
+    public MusicEntity() {
+
+    }
+
+    public MusicEntity(String songName, String songURL) {
+        this.songName = songName;
+        this.songURL = songURL;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public String getSongURL() {
+        return songURL;
+    }
 }
